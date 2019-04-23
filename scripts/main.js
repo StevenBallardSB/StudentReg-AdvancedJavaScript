6efef4fc6b286a40b315fcb53e130eba0e05b9a5
STEVEN Ballard <ballard9576@students.cptc.edu>
2019-04-23 14:53:09 -0700

    main.js was created

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var Student = (function () {
+    function Student() {
+    }
+    return Student;
+}());
+var testStu = new Student();
+testStu.firstName = "Joe";
+testStu.lastName = "Ortiz";
+testStu.dateOfBirth = new Date(1980, 3, 1);
+window.onload = function () {
+    var regBtn = document.querySelector("main > button");
+    regBtn.onclick = registerStudent;
+};
+function registerStudent() {
+    var nextStudent = getStudent();
+    displayStudent(nextStudent);
+}
+function getStudent() {
+    var s = new Student();
+    s.firstName = document.getElementById("first-name").value;
+    s.lastName = document.getElementById("last-name").value;
+    return s;
+}
+function displayStudent(stu) {
+    alert(stu.firstName + " " + stu.lastName);
+}
